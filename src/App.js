b2510fc32f5c499016498f9774e749d2b9f2a9ef
omerbasri <omerbasriokcu@gmail.com>
2024-02-26 20:34:01 +0300

    add search bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import SearchBar from "./components/SearchBar";
+import WeatherLogo from "./components/WeatherLogo";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <>
+      <div className="App">
+        <Navbar></Navbar>
+        <WeatherLogo></WeatherLogo>
+      </div>
+      <div>
+        <SearchBar></SearchBar>
+      </div>
+    </>
   );
 }
 
